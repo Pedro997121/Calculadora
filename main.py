23b3f0fd0e04852da09d4f0882260b632906bc36
Pedro <ussanezio@hotmail.com>
2022-07-20 17:14:38 -0300

    Segunda janela

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from tkinter import ttk
 
 window = Tk()
 window.title("Calculadora")
-window.geometry("800x600")
+window.geometry("300x500")
+
+window_frame = Frame(window, width=300, height=80,bg="#db1212")
+window_frame.grid(row=0, column=0)
+
 window.mainloop()
